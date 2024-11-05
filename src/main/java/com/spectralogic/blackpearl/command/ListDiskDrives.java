b1c9e34191a14f079e7c4af50935201c7b006765
etruyj <etruyj@gmail.com>
2024-11-05 17:45:26 -0500

    Fixed bug where a null arraylist of data disks is returned if no disks are present. This was causing code upstream to crash.

--- src/main/java/com/spectralogic/blackpearl/command/ListDiskDrives.java
+++ src/main/java/com/spectralogic/blackpearl/command/ListDiskDrives.java
@@ -47,15 +47,16 @@ public class ListDiskDrives {
         try {
             all_drives = pearl.listDataDisks();
 
-            for(DiskDrive drive : all_drives) {
-                // check to see if the drive exists (product != null)
-                // if the drive is not a bezel (bezel == false)
-                // and the drive isn't assigned to a pool.
-                if(drive.getPoolId() == null && drive.getProduct() != null && drive.getBezel() == false) {
-                    drive_list.add(drive);
+            if(all_drives != null) {
+                for(DiskDrive drive : all_drives) {
+                    // check to see if the drive exists (product != null)
+                    // if the drive is not a bezel (bezel == false)
+                    // and the drive isn't assigned to a pool.
+                    if(drive.getPoolId() == null && drive.getProduct() != null && drive.getBezel() == false) {
+                        drive_list.add(drive);
+                    }
                 }
             }
-
             log.info("Found (" + drive_list.size() + ") available data drives.");
         } catch(Exception e) {
             log.error(e.getMessage());
@@ -75,22 +76,24 @@ public class ListDiskDrives {
 
         try {
             all_drives = all(pearl);
+            
+            if(all_drives != null) {
+               for(DiskDrive drive : all_drives) {
+                    // check to see if the drive exists (product != null)
+                    // if the drive is not a bezel (bezel == false)
+                    // and the drive isn't assigned to a pool.
+                    if(drive.getPoolId() == null && drive.getProduct() != null && drive.getBezel() == false) {
+                        map_key = drive.getInterface() + ":" + drive.getSpeed();
 
-            for(DiskDrive drive : all_drives) {
-                // check to see if the drive exists (product != null)
-                // if the drive is not a bezel (bezel == false)
-                // and the drive isn't assigned to a pool.
-                if(drive.getPoolId() == null && drive.getProduct() != null && drive.getBezel() == false) {
-                    map_key = drive.getInterface() + ":" + drive.getSpeed();
-
-                    if(drive_type_map.get(map_key) != null) {
-                        // Get the corresponding array list and add a drive
-                        // to the referenced value without assignment. Java is fun.
-                        drive_type_map.get(map_key).add(drive);
-                    } else {
-                        drive_list = new ArrayList<DiskDrive>();
-                        drive_list.add(drive);
-                        drive_type_map.put(map_key, drive_list);
+                        if(drive_type_map.get(map_key) != null) {
+                            // Get the corresponding array list and add a drive
+                            // to the referenced value without assignment. Java is fun.
+                            drive_type_map.get(map_key).add(drive);
+                        } else {
+                            drive_list = new ArrayList<DiskDrive>();
+                            drive_list.add(drive);
+                            drive_type_map.put(map_key, drive_list);
+                        }
                     }
                 }
             }
@@ -99,16 +102,21 @@ public class ListDiskDrives {
             log.info("Returning drive type with the largest number of drives.");
             drive_list = null;
 
-            for(String key : drive_type_map.keySet()) {
-                // If the list is null (first iteration or the list is smaller than the map(key) list
-                // the new key is stored as the potential return value. 
-                if(drive_list == null || drive_list.size() < drive_type_map.get(key).size()) {
-                    drive_list = drive_type_map.get(key);
-                    map_key = key; // store this for logging.
+            if(drive_type_map.size() > 0) {
+                for(String key : drive_type_map.keySet()) {
+                    // If the list is null (first iteration or the list is smaller than the map(key) list
+                    // the new key is stored as the potential return value. 
+                    if(drive_list == null || drive_list.size() < drive_type_map.get(key).size()) {
+                        drive_list = drive_type_map.get(key);
+                        map_key = key; // store this for logging.
+                    }
                 }
-            }
             
-            log.info("Returning (" + drive_list.size() + ") " + map_key + " disk drives.");
+                log.info("Returning (" + drive_list.size() + ") " + map_key + " disk drives.");
+            } else {
+                log.warn("No data drive types are available.");
+                return new ArrayList<DiskDrive>();
+            }
         } catch(Exception e) {
             log.error(e.getMessage());
             log.error("Failed to list disk drives.");
